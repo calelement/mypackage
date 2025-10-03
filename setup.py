4f86d2fe9e038594a88b0da75d1d42425d83b809
Caleb Odhiambo <codhiamboclement@gmail.com>
2025-10-03 18:25:40 +0300

    Updated name and repository in setup file

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
-    author='<Your Name>',
+    url='https://github.com/calelement/mypackage',
+    author='<Caleb Odhiambo>',
     author_email='<Your Email>'
 )
